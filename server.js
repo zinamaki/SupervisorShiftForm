8f74e9c8d7981ead8c42a7d519e058d8bb99a53a
Mina Zaki <minazaki@Minas-MacBook-Pro.local>
2017-03-02 14:57:14 -0500

    Completed test site

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-//Lets require/import the HTTP module
-var http = require('http');
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-//Lets define a port we want to listen to
-const PORT=3000; 
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
-    console.log("Server listening on: http://localhost:%s", PORT);
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+	console.log("Listening on " + port);
 });
