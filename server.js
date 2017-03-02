f8c266d199d801288b7409d17e8ab7737ec01072
Mina Zaki <minazaki@Minas-MacBook-Pro.local>
2017-03-02 17:08:18 -0500

    Added testing site

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+//var http = require('http');
+//
+//http.createServer(function (request, response) {
+//  response.writeHead(200, {'Content-Type': 'text/plain'});
+//  response.end('testing. . .\n');
+//}).listen(3000);
+//
+//console.log('Server running at http://127.0.0.1:8124/');
+
+
+
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
@@ -6,10 +17,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+        res.sendFile(__dirname + '/index.html');
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-	console.log("Listening on " + port);
+        console.log("Listening on " + port);
 });
+
+
+
+
+
